8cc8411700861bf51973785efc1d12eec08a53df
Jtomat <stragnik13@gmail.com>
2024-01-27 12:48:58 +0500

    feat: add 3d driver

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -12,7 +12,7 @@ figure_list: list = json.load(open('static/data.json', encoding='utf-8'))['figur
 # функция, которая возвращает то, что надо отобразить по указанному ранее маршруту
 def index():
     # используя обработчик шаблона обрабатываем шаблон главной страницы (main.html) и возвращаем результат пользователю
-    return render_template("main.html")
+    return render_template("main.html", figures=list(map(lambda x: {"id": x["id"], "name": x["name"] },figure_list)) )
 # создаем маршрут доступный по пути '/figures/<figure>', где <figure> - уникальный ключ материала
 # этот маршрут может только возвращать данные (так как мы указали метод доступа 'GET')
 @app.route("/figures/<figure>", methods=['GET'])
@@ -22,8 +22,8 @@ def figure_page(figure):
     # используя обработчик шаблона обрабатываем шаблон страницы материала (figure.html),
     # предварительно указав, что в качестве переменной figure надо использовать элемент списка figure_list,
     # лежащий в позиции (уникальный ключ как число минус 1, так как список нумеруется с 0)
-    return render_template("figure.html", figure=figure_list[int(figure) - 1])
+    return render_template("figure.html", figure=figure_list[int(figure) - 1], figures=list(map(lambda x: {"id": x["id"], "name": x["name"] },figure_list)) )
 
 # если этот файл запускается самостоятельно, то включить приложение
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
